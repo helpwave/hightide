985acae01a0f6309a908147165cafbc3fcc112a5
DasProffi <67233923+DasProffi@users.noreply.github.com>
2025-06-23 13:24:57 +0200

    publish: version 0.1.4

--- src/components/layout-and-navigation/StepperBar.tsx
+++ src/components/layout-and-navigation/StepperBar.tsx
@@ -1,10 +1,11 @@
 import { Check, ChevronLeft, ChevronRight } from 'lucide-react'
-import type { Language } from '../../localization/util'
-import type { PropsForTranslation } from '../../localization/useTranslation'
-import { useTranslation } from '../../localization/useTranslation'
-import { range } from '../../util/array'
+import type { Language } from '@/localization/util'
+import type { PropsForTranslation } from '@/localization/useTranslation'
+import { useTranslation } from '@/localization/useTranslation'
+import { range } from '@/util/array'
 import { SolidButton } from '../user-action/Button'
 import clsx from 'clsx'
+import { useEffect, useState } from 'react'
 
 type StepperBarTranslation = {
   back: string,
@@ -25,18 +26,25 @@ const defaultStepperBarTranslation: Record<Language, StepperBarTranslation> = {
   }
 }
 
-export type StepperInformation = {
-  step: number,
-  lastStep: number,
+export type StepperState = {
+  currentStep: number,
   seenSteps: Set<number>,
 }
 
 export type StepperBarProps = {
-  stepper: StepperInformation,
-  onChange: (step: StepperInformation) => void,
+  state?: StepperState,
+  numberOfSteps: number,
+  disabledSteps: Set<number>,
+  onChange: (state: StepperState) => void,
   onFinish: () => void,
+  finishText?: string,
   showDots?: boolean,
   className?: string,
+}
+
+const defaultState: StepperState = {
+  currentStep: 0,
+  seenSteps: new Set([0])
 }
 
 /**
@@ -44,30 +52,33 @@ export type StepperBarProps = {
  */
 export const StepperBar = ({
                              overwriteTranslation,
-                             stepper,
+                             state,
+                             numberOfSteps,
+                             disabledSteps = new Set(),
                              onChange,
                              onFinish,
+                             finishText,
                              showDots = true,
                              className = '',
                            }: PropsForTranslation<StepperBarTranslation, StepperBarProps>) => {
   const translation = useTranslation(defaultStepperBarTranslation, overwriteTranslation)
-  const dots = range(0, stepper.lastStep)
-  const { step, seenSteps, lastStep } = stepper
+  const dots = range(0, numberOfSteps)
+  const { currentStep, seenSteps } = state ?? defaultState
 
   const update = (newStep: number) => {
     seenSteps.add(newStep)
-    onChange({ step: newStep, seenSteps, lastStep })
+    onChange({ currentStep: newStep, seenSteps })
   }
 
   return (
     <div
-      className={clsx('sticky row p-2 border-2 justify-between rounded-lg shadow-lg', className)}
+      className={clsx('row justify-between',className)}
     >
-      <div className="flex-[2] justify-start">
+      <div className="row flex-[2] justify-start">
         <SolidButton
-          disabled={step === 0}
+          disabled={currentStep === 0 || disabledSteps.has(currentStep)}
           onClick={() => {
-            update(step - 1)
+            update(currentStep - 1)
           }}
           className="row gap-x-1 items-center justify-center"
         >
@@ -83,42 +94,61 @@ export const StepperBar = ({
               key={index}
               onClick={() => seen && update(index)}
               className={clsx('rounded-full w-4 h-4', {
-                  'bg-primary hover:brightness-75': index === step && seen,
-                  'bg-primary/40 hover:bg-primary': index !== step && seen,
-                  'bg-gray-200 outline-transparent': !seen,
+                  'bg-primary hover:brightness-75': index === currentStep && seen && !disabledSteps.has(currentStep),
+                  'bg-primary/40 hover:bg-primary': index !== currentStep && seen && !disabledSteps.has(currentStep),
+                  'bg-gray-200 outline-transparent': !seen || disabledSteps.has(currentStep),
                 },
                 {
                   'cursor-pointer': seen,
-                  'cursor-not-allowed': !seen,
+                  'cursor-not-allowed': !seen || disabledSteps.has(currentStep),
                 })}
             />
           )
         })}
       </div>
-      {step !== lastStep && (
-        <div className="flex-[2] justify-end">
+      {currentStep !== numberOfSteps && (
+        <div className="row flex-[2] justify-end">
           <SolidButton
-            onClick={() => update(step + 1)}
+            onClick={() => update(currentStep + 1)}
             className="row gap-x-1 items-center justify-center"
+            disabled={disabledSteps.has(currentStep)}
           >
             {translation.next}
             <ChevronRight size={14}/>
           </SolidButton>
         </div>
       )}
-      {step === lastStep && (
-        <div className="flex-[2] justify-end">
+      {currentStep === numberOfSteps && (
+        <div className="row flex-[2] justify-end">
           <SolidButton
-            // TODO check form validity
-            disabled={false}
+            disabled={disabledSteps.has(currentStep)}
             onClick={onFinish}
             className="row gap-x-1 items-center justify-center"
           >
             <Check size={14}/>
-            {translation.confirm}
+            {finishText ?? translation.confirm}
           </SolidButton>
         </div>
       )}
     </div>
+  )
+}
+
+export const StepperBarUncontrolled = ({ state, onChange, ...props }: StepperBarProps) => {
+  const [usedState, setUsedState] = useState<StepperState>(state ?? defaultState)
+
+  useEffect(() => {
+    setUsedState(state ?? defaultState)
+  }, [state])
+
+  return (
+    <StepperBar
+      {...props}
+      state={usedState}
+      onChange={newState => {
+        setUsedState(newState)
+        onChange(newState)
+      }}
+    />
   )
 }
