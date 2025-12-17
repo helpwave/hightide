6627d96d1c5e92d6a892e91acfbe366f9fcc0756
DasProffi <67233923+DasProffi@users.noreply.github.com>
2025-12-18 00:02:35 +0100

    fix: fix tooltip

--- stories/user-action/tooltip/TooltipMany.stories.tsx
+++ stories/user-action/tooltip/TooltipMany.stories.tsx
@@ -1,15 +1,20 @@
 import type { Meta, StoryObj } from '@storybook/nextjs'
 import type { TooltipProps } from '../../../src/components/user-action/Tooltip'
 import { Tooltip } from '../../../src/components/user-action/Tooltip'
+import { range } from '../../../src'
 
 type TooltipExampleProps = Omit<TooltipProps, 'children' | 'tooltip'> & { tooltipText: string }
 
 const TooltipExample = ({ tooltipText, ...props }: TooltipExampleProps) => {
   return (
-    <div className="flex-col-2 items-center justify-center min-h-64">
-      <Tooltip tooltip={tooltipText} {...props}>
-        <span className="bg-primary text-white px-2 py-1 rounded-lg">Hover over me</span>
-      </Tooltip>
+    <div className="flex-col-2 items-center">
+      {range(10).map((index) => {
+        return (
+          <Tooltip key={index} tooltip={tooltipText + index} {...props}>
+            <span className="bg-primary text-white px-2 py-1 rounded-lg">{'Hover over me '  + index}</span>
+          </Tooltip>
+        )
+      })}
     </div>
   )
 }
@@ -22,7 +27,7 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>;
 
-export const tooltip: Story = {
+export const tooltipMany: Story = {
   args: {
     tooltipText: 'Tooltip',
     appearDelay: 500,
